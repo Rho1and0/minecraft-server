84bc8f003a9de1254fda07fdaae3efd3ce2ad391
Rolando Monzon <rho1and0@hotmail.com>
2026-08-05 04:17:54 -0400

    segundo intento

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,7 @@ RUN apt-get update && \
     apt-get install -y curl jq && \
     rm -rf /var/lib/apt/lists/*
 
-COPY download-plugins.sh /download-plugins.sh
-RUN chmod +x /download-plugins.sh
+COPY download-plugins.sh /usr/local/bin/download-plugins.sh
+RUN chmod +x /usr/local/bin/download-plugins.sh
 
-USER minecraft
-
-ENTRYPOINT ["/download-plugins.sh"]
+ENTRYPOINT ["/usr/local/bin/download-plugins.sh"]
--- download-plugins.sh
+++ download-plugins.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
-
 set -e
 
 PLUGIN_DIR="/data/plugins"
+
 mkdir -p "$PLUGIN_DIR"
 
-download_modrinth_plugin() {
+URL=$(curl -s https://api.modrinth.com/v2/project/heartbound/version \
+    | jq -r '.[0].files[0].url')
 
-    PROJECT_ID="$1"
-    OUTPUT_NAME="$2"
+curl -L "$URL" -o "$PLUGIN_DIR/Heartbound.jar"
 
-    echo "Buscando última versión de $PROJECT_ID..."
+chown -R minecraft:minecraft /data
 
-    URL=$(curl -s "https://api.modrinth.com/v2/project/${PROJECT_ID}/version" \
-        | jq -r '.[0].files[0].url')
-
-    if [[ "$URL" == "null" || -z "$URL" ]]; then
-        echo "No se pudo obtener $PROJECT_ID"
-        exit 1
-    fi
-
-    if [ ! -f "$PLUGIN_DIR/$OUTPUT_NAME.jar" ]; then
-        echo "Descargando $OUTPUT_NAME..."
-        curl -L "$URL" -o "$PLUGIN_DIR/$OUTPUT_NAME.jar"
-    else
-        echo "$OUTPUT_NAME ya existe."
-    fi
-}
-
-download_modrinth_plugin "heartbound" "Heartbound"
-
-echo "Plugins listos."
-
-exec /start
+exec su-exec minecraft /start
