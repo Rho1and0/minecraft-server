ea68154262a3d260a3e11057512a7270e765d403
Rolando Monzon <rho1and0@hotmail.com>
2026-08-06 15:09:58 -0400

    agregando mod WorldReset

--- download-plugins.sh
+++ download-plugins.sh
@@ -15,13 +15,13 @@ if [ ! -f "$PLUGIN_DIR/Heartbound.jar" ]; then
     chown -R 1000:1000 "$PLUGIN_DIR"
 fi
 
-if [ ! -f "$PLUGIN_DIR/RandomSPAWNZ.jar" ]; then
-    echo "Descargando RandomSPAWNZ..."
+if [ ! -f "$PLUGIN_DIR/WorldReset.jar" ]; then
+    echo "Descargando WorldReset..."
 
-    URL=$(curl -s https://api.modrinth.com/v2/project/randomspawnz/version \
+    URL=$(curl -s https://api.modrinth.com/v2/project/worldreset/version \
         | jq -r '.[0].files[0].url')
 
-    curl -L "$URL" -o "$PLUGIN_DIR/RandomSPAWNZ.jar"
+    curl -L "$URL" -o "$PLUGIN_DIR/WorldReset.jar"
 
     chown -R 1000:1000 "$PLUGIN_DIR"
 fi
