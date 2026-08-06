26420f1697a46bbf748930f42ab1298ae89ebf36
Rolando Monzon <rho1and0@hotmail.com>
2026-08-06 14:17:31 -0400

    descargar RandomSPAWNZ

--- download-plugins.sh
+++ download-plugins.sh
@@ -14,3 +14,14 @@ if [ ! -f "$PLUGIN_DIR/Heartbound.jar" ]; then
 
     chown -R 1000:1000 "$PLUGIN_DIR"
 fi
+
+if [ ! -f "$PLUGIN_DIR/RandomSPAWNZ.jar" ]; then
+    echo "Descargando RandomSPAWNZ..."
+
+    URL=$(curl -s https://api.modrinth.com/v2/project/randomspawnz/version \
+        | jq -r '.[0].files[0].url')
+
+    curl -L "$URL" -o "$PLUGIN_DIR/RandomSPAWNZ.jar"
+
+    chown -R 1000:1000 "$PLUGIN_DIR"
+fi
